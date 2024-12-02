cf5e3ad6f49521374e2942c300f2b90a35acd027
hp15f <hp15f@LAPTOP-1G0PTJV1>
2024-12-02 08:07:51 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿        int rows = 4;
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= rows - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 1; i <= 3; i++)
+        {
+            for (int j = 1; j <= rows - 1; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("*");
+        }
